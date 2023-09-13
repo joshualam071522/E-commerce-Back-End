7d52e51055942f9f034a17fff4233be478566b24
Joshua Lam <joshualam071522@gmail.com>
2023-09-13 01:59:40 -0700

    added readme, fixed typos in routes

--- README.md
+++ README.md
@@ -1,1 +1,45 @@
 # E-commerce-Back-End
+
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+
+
+Link to walkthrough video: https://drive.google.com/file/d/1_xvTK71wnwdcJLNhmwAgkLoEIgrQax3C/view
+
+## Table of Contents
+
+- [Description](#description)
+- [Installation](#installation)
+- [Usage](#usage)
+- [Credits](#credits)
+- [Tests](#tests)
+- [Questions](#questions)
+
+## Description
+
+The purpose of this project was to use a mysql database along with express to make a back-end application for a e-commerce website. This project helped reinforce my knowledge on the use of how my sql databases works with sequelize models and linking the models using associations and references. Using express also helps shows the understanding of how to retrieve and send information from and to the mysql database.
+
+## Installation
+
+To be able to run the application, you first need to install all the packages by typing "npm install", and making sure you have mysql installed on your computer. You must also see up your .env file to access the database securely. You will also need an API client like Insomnia to test out the routes.
+
+## Usage
+
+To use this application, simply open up your API client. Now you can test out the routes for categories, products, and tags. the default endpoints should be localhost:3001/api/.
+To access the categories route, simply add in category, simply use the endpoint "localhost:3001/api/categories/". Same goes for products and tags, except you replace the categories parameter with their respective routes. More information on which routes are available can be seen in their respective route files located in the routes/api folder.
+
+## Credits
+
+https://github.com/joshualam071522
+
+## License
+
+This project is licensed under the MIT license.
+https://opensource.org/licenses/MIT
+
+## Tests
+
+N/A
+
+## Questions
+
+To reach me for additional questions, please send an email to: privateemail@email.com
--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -3,26 +3,78 @@ const { Tag, Product, ProductTag } = require('../../models');
 
 // The `/api/tags` endpoint
 
-router.get('/', (req, res) => {
-  // find all tags
-  // be sure to include its associated Product data
+// finds all tags
+router.get('/', async (req, res) => {
+  try{
+    const tagData = await Tag.findAll({
+      include: [{model: Product}]
+    });
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.get('/:id', (req, res) => {
-  // find a single tag by its `id`
-  // be sure to include its associated Product data
+// find tag by id
+router.get('/:id', async (req, res) => {
+  try {
+  const tagData = await Tag.findByPk(req.params.id, {
+    include: [{model: Product}]
+  });
+  if (!tagData) {
+    res.status(404).json({message: 'No tag found with that id!'});
+    return;
+  }
+  res.status(200).json(tagData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.post('/', (req, res) => {
-  // create a new tag
+// creates new tag
+router.post('/', async (req, res) => {
+  try {
+    const newTagData = await Tag.create(req.body);
+    res.status(200).json(newTagData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.put('/:id', (req, res) => {
-  // update a tag's name by its `id` value
+// Update tag name by id
+router.put('/:id', async (req, res) => {
+  try {
+    const updatedTagData = await Tag.update(req.body, {
+      where: {
+        id: req.params.id,
+      },
+    });
+    if (!updatedTagData) {
+      res.status(404).json({message: 'No tag found with that id!'});
+      return;
+    }
+    res.status(200).json({updatedTagData, message: 'Tag updated!'});
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.delete('/:id', (req, res) => {
-  // delete on tag by its `id` value
+// Delete tag by id
+router.delete('/:id', async (req, res) => {
+  try {
+    const deletedTagData = await Tag.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+    if (!deletedTagData) {
+      res.status(404).json({message: 'No tag found with that id!'});
+      return;
+    }
+    res.status(200).json({deletedTagData, message: 'Tag deleted!'});
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
